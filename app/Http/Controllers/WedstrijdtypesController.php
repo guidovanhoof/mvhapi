baa40a3c3b72058634ce14ed54378aa4212464c8
Guido Van Hoof <guido.van.hoof3@gmail.com>
2021-02-04 10:28:49 +0100

    Verwijderen wedstrijdtype niet mogelijk indien nog gekoppeld aan wedstrijd(en)

--- app/Http/Controllers/WedstrijdtypesController.php
+++ app/Http/Controllers/WedstrijdtypesController.php
@@ -5,11 +5,13 @@ namespace App\Http\Controllers;
 use App\Http\Resources\WedstrijdtypeResource;
 use App\Models\Wedstrijdtype;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use Illuminate\Http\Response;
 use function App\Helpers\nietGevondenResponse;
+use function App\Helpers\nietVerwijderdResponse;
 use function App\Helpers\verwijderdResponse;
 use const App\Helpers\STORING;
 use const App\Helpers\UPDATING;
@@ -84,7 +86,11 @@ class WedstrijdtypesController extends Controller
     {
         try {
             $wedstrijdtype = Wedstrijdtype::where("id", $id)->firstOrFail();
-            $wedstrijdtype->delete();
+            try {
+                $wedstrijdtype->delete();
+            } catch (QueryException $queryException) {
+                return nietVerwijderdResponse("Wedstrijdtype", "wedstrijden");
+            }
             return verwijderdResponse("Wedstrijdtype");
         } catch (ModelNotFoundException $modelNotFoundException) {
             return nietGevondenResponse("Wedstrijdtype");
