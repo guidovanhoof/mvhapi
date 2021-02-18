9f2ba0d1541151db5d86d54321acbc4c3a58cdc3
Guido Van Hoof <guido.van.hoof3@gmail.com>
2021-02-18 19:18:39 +0100

    Verwijderen bestaande deelnemer

---
+++ tests/Feature/Admin/Deelnemers/DeelnemersDestroyTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Tests\Feature\Admin\Deelnemers;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Testing\TestResponse;
+use Tests\TestCase;
+
+class DeelnemersDestroyTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function tearDown(): void
+    {
+        cleanUpDb("deelnemers");
+
+        parent::tearDown();
+    }
+
+    /** @test */
+    public function deelnemerNietAanwezig()
+    {
+        $response = $this->verwijderDeelnemer(666);
+
+        $response->assertStatus(404);
+        $errorMessage = $response->json()["message"];
+        $this->assertEquals("Deelnemer niet gevonden!", $errorMessage);
+    }
+
+    /** @test */
+    public function deelnemerAanwezig()
+    {
+        $deelnemer = bewaarDeelnemer();
+
+        $response = $this->verwijderDeelnemer($deelnemer->nummer);
+
+        $response->assertStatus(200);
+        $errorMessage = $response->json()["message"];
+        $this
+            ->assertDatabaseMissing(
+                "deelnemers",
+                deelnemerToArry($deelnemer)
+            )
+            ->assertEquals("Deelnemer verwijderd!", $errorMessage)
+        ;
+    }
+
+    /**
+     * @param $nummer
+     * @return TestResponse
+     */
+    private function verwijderDeelnemer($nummer): TestResponse
+    {
+        $plainToken = createUserAndToken();
+
+        return
+            $this
+                ->withHeader('Authorization', 'Bearer ' . $plainToken)
+                ->json(
+                    'DELETE',
+                    URL_DEELNEMERS_ADMIN . $nummer
+                )
+            ;
+    }
+}
