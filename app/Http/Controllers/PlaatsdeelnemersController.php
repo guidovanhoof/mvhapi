740f8a42fd50cc0eb1a380c69d0b5143fc04fc2e
Guido Van Hoof <guido.van.hoof3@gmail.com>
2021-02-24 18:33:06 +0100

    Ophalen één plaatsdeelnemer

--- app/Http/Controllers/PlaatsdeelnemersController.php
+++ app/Http/Controllers/PlaatsdeelnemersController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\PlaatsdeelnemerResource;
 use  App\Models\Plaatsdeelnemer;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use function App\Helpers\nietGevondenResponse;
 
 class PlaatsdeelnemersController extends Controller
 {
@@ -23,17 +25,7 @@ class PlaatsdeelnemersController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
+     * Bewaren nieuwe plaatsdeelnemer..
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
@@ -44,29 +36,23 @@ class PlaatsdeelnemersController extends Controller
     }
 
     /**
-     * Display the specified resource.
+     * Ophalen één plaatsdeelnemer.
      *
-     * @param  \App\Models\Plaatsdeelnemer  $plaatsdeelnemer
-     * @return \Illuminate\Http\Response
+     * @param  $id
+     * @return JsonResponse
      */
-    public function show(Plaatsdeelnemer $plaatsdeelnemer)
+    public function show($id): JsonResponse
     {
-        //
+        try {
+            $plaatsdeelnemer = Plaatsdeelnemer::where("id", $id)->firstOrFail();
+            return $this->plaatsdeelnemerResourceResponse($plaatsdeelnemer, 200);
+        } catch(ModelNotFoundException $modelNotFoundException) {
+            return nietGevondenResponse("Plaatsdeelnemer");
+        }
     }
 
     /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Plaatsdeelnemer  $plaatsdeelnemer
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Plaatsdeelnemer $plaatsdeelnemer)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
+     * Wijzigen bestaande plaatsdeelnemer.
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \App\Models\Plaatsdeelnemer  $plaatsdeelnemer
@@ -78,7 +64,7 @@ class PlaatsdeelnemersController extends Controller
     }
 
     /**
-     * Remove the specified resource from storage.
+     * Verwijderen bestaande plaatsdeelnemer.
      *
      * @param  \App\Models\Plaatsdeelnemer  $plaatsdeelnemer
      * @return \Illuminate\Http\Response
@@ -86,5 +72,18 @@ class PlaatsdeelnemersController extends Controller
     public function destroy(Plaatsdeelnemer $plaatsdeelnemer)
     {
         //
+    }
+
+    /**
+     * @param Plaatsdeelnemer $plaatsdeelnemer
+     * @param int $status
+     * @return JsonResponse
+     */
+    private function plaatsdeelnemerResourceResponse(Plaatsdeelnemer $plaatsdeelnemer, int $status): JsonResponse
+    {
+        return response()->json(
+            new PlaatsdeelnemerResource($plaatsdeelnemer),
+            $status
+        );
     }
 }
